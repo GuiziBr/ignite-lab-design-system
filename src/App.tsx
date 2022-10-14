642489d96cddaf447c7731a0bff4f504a58dd625
Ricardo Guizi <guizi189@gmail.com>
2022-10-13 19:05:03 -0700

    Design system example

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className='font-bold-2xl text-violet-500'>Hello World</h1>
+
+      <button className='bg-cyan-500 font-medium px-4 rounded text-white hover:bg-cyan-300'>Send</button>
+    </>
+  )
+}
